5568c19d3d26c5a0acbe8343fbb073824b3ea4f3
hellocoldwinter <57827991+hellocoldwinter@users.noreply.github.com>
2021-04-01 00:43:12 -0400

    change js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,72 @@
-var video;
+var video = document.getElementById("player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	var slider = document.getElementById("slider");
+	var volume = document.getElementById("volume");
+	volume.innerHTML = slider.value + " %";
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate - video.playbackRate*0.05;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate + video.playbackRate*0.05;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime == video.duration) {
+		console.log("Original location " + video.currentTime);
+		video.currentTime = 0
+		console.log("Going back to beginning");
+	}
+	else {
+		console.log("Original location " + video.currentTime);
+		video.currentTime = video.currentTime + 15;
+		console.log("New location " + video.currentTime);
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted;
+	if (video.muted) {
+		console.log("Video is muted");
+	}
+	else {
+		console.log("Video is unmuted");
+	}
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+	var slider = document.getElementById("slider");
+	var volume = document.getElementById("volume");
+	volume.innerHTML = slider.value;
+	
+	slider.oninput = function() {
+  	volume.innerHTML = this.value + " %";
+	}
+	video.volume = volume.innerHTML/100;
+	console.log("Volume is " + volume.innerHTML + " %");
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool")
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool")
+});
